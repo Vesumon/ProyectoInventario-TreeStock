cce92b490793f7d9ab112acd7537c8f987b254a6
Vesumon <proyeremy12349@gmail.com>
2026-04-02 18:33:22 -0500

    Agregamos recorrido inorden y busqueda en el arbol

--- ArbolInventario.java
+++ ArbolInventario.java
@@ -23,4 +23,43 @@ public class ArbolInventario {
     public void insertar(int id, String nombre) {
         raiz = insertarRecursivo(raiz, id, nombre);
     }
+
+    // Recorrido Inorden (muestra ordenado)
+    public void recorridoInorden(producto raiz) {
+        
+        if (raiz != null) {
+            
+            recorridoInorden(raiz.izquierdo);
+
+            System.out.println("ID: " + raiz.id + " Nombre: " + raiz.nombre);
+
+            recorridoInorden(raiz.derecho);
+        }
+    }
+
+    public void mostrarInventario(){
+        recorridoInorden(raiz);
+    }
+
+    // Buscar producto
+    public boolean buscar(producto raiz, int id){
+        
+        if (raiz == null) {
+            return false;
+        }
+
+        if (raiz.id == id) {
+            return true;
+        }
+
+        if (id < raiz.id){
+            return buscar(raiz.izquierdo, id);
+        } else {
+            return buscar(raiz.derecho, id);
+        }
+    }
+
+    public boolean buscarProducto(int id){
+        return buscar(raiz, id);
+    }
 }
